07f098afcb80b05f7c9d38cb6bd913d7c9510f61
rvsmooth <riveeks.smooth@gmail.com>
2026-02-02 09:23:10 +0530

    feat(build.sh): exit in case there is no linux binary available

--- build.sh
+++ build.sh
@@ -7,27 +7,35 @@ FORKGRAM_URL="https://github.com/forkgram/tdesktop/releases/download/${TAG}/Tele
 FORKGRAM="forkgram.tar.xz"
 DIR_STR="AppDir/usr/bin"
 
-# get deps
-wget -O "$IMGTOOL" "$IMGTOOL_URL"
-wget -O "$FORKGRAM" "$FORKGRAM_URL"
+build() {
+  # get deps
+  wget -O "$IMGTOOL" "$IMGTOOL_URL"
 
-# make IMGTOOL executable
-chmod +x "$IMGTOOL"
+  # make IMGTOOL executable
+  chmod +x "$IMGTOOL"
 
-# Create directory structure
-mkdir -p "$DIR_STR"
+  # Create directory structure
+  mkdir -p "$DIR_STR"
 
-# Extract forkgram archive
-tar -xf "$FORKGRAM"
+  # Extract forkgram archive
+  tar -xf "$FORKGRAM"
 
-# Prepare AppDir
-mv Telegram "$DIR_STR"/forkgram
-cp forkgram.desktop AppDir/
-cp run.sh AppDir/AppRun
-cp org.forkgram.desktop.png AppDir/
+  # Prepare AppDir
+  mv Telegram "$DIR_STR"/forkgram
+  cp forkgram.desktop AppDir/
+  cp run.sh AppDir/AppRun
+  cp org.forkgram.desktop.png AppDir/
 
-# Build
-ARCH=x86_64 ./"$IMGTOOL" AppDir forkgram.AppImage
+  # Build
+  ARCH=x86_64 ./"$IMGTOOL" AppDir forkgram.AppImage
 
-# Export verison to github
-echo "FORKGRAM_VERSION=$TAG" >>$GITHUB_ENV
+  # Export verison to github
+  echo "FORKGRAM_VERSION=$TAG" >>$GITHUB_ENV
+}
+
+# exit if linux binary is not available on the remote repository
+wget -qO "$FORKGRAM" "$FORKGRAM_URL" || {
+  echo "Error: Didn't find forkgram-$TAG for linux on remote repo"
+  exit 1
+}
+build
